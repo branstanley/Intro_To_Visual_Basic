20502538403c964f0988ec002f09eb6cf627ce44
Brandon Stanley <bstanley@lakeheadu.ca>
2015-04-19 16:41:28 -0400

    Intro to classes and properties
    Nothing interesting, extended the hello world program to use a class
    with properties.

--- Intro_To_Visual_Basic/Driver.vb
+++ Intro_To_Visual_Basic/Driver.vb
@@ -1,7 +1,10 @@
 ﻿Module Hello
     Sub Main()
         Dim someVar As String = "Hello world!"
-        MsgBox(someVar)
+        Dim hello_world_class As HelloWorldClass = New HelloWorldClass()
+
+        hello_world_class.HelloString = someVar
+
+        MsgBox(hello_world_class.HelloString & " is " & hello_world_class.Length() & " characters long")
     End Sub
 End Module
-
---
+++ Intro_To_Visual_Basic/HelloWorldClass.vb
@@ -0,0 +1,19 @@
+﻿Public Class HelloWorldClass
+    Private hello_string As String
+
+    Property HelloString As String
+        Get
+            Return hello_string
+        End Get
+        Set(value As String)
+            hello_string = value
+        End Set
+    End Property
+
+    ReadOnly Property Length() As Integer
+        Get
+            Return hello_string.Length
+        End Get
+    End Property
+
+End Class
