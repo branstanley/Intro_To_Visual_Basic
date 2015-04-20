69a6515058b8edfe6d4ec034a0e4f93b18d93e5d
Brandon Stanley <bstanley@lakeheadu.ca>
2015-04-20 01:18:39 -0400

    Comments

--- Intro_To_Visual_Basic/Driver.vb
+++ Intro_To_Visual_Basic/Driver.vb
@@ -1,18 +1,34 @@
 ﻿Module Hello
     Sub Main()
+        ' Setting a variable
         Dim someVar As String = "Hello world!"
+        ' Creating a class
         Dim hello_world_class As HelloWorldClass = New HelloWorldClass()
 
+        ' Practicing using a property
         hello_world_class.HelloString = someVar
-
-        MsgBox(hello_world_class.HelloString & " is " & hello_world_class.Length() & " characters long")
+        MsgBox(hello_world_class.HelloString & " is " & hello_world_class.Length & " characters long")
 
         someVar = ""
+
+        ' Practicing For Each loop and functions that return a list
         For Each tempString As String In hello_world_class.hello_func()
             someVar = someVar & tempString
         Next
 
         MsgBox(someVar)
+
+        ' Practice for loop
+        For i As Integer = 0 To 50
+            Console.Write(i & " ")
+        Next
+        Console.WriteLine()
+
+        ' Practive for loop that increments by 2
+        For i As Integer = 0 To 50 Step 2
+            Console.Write(i & " ")
+        Next
+        Console.WriteLine()
 
     End Sub
 End Module
--- Intro_To_Visual_Basic/HelloWorldClass.vb
+++ Intro_To_Visual_Basic/HelloWorldClass.vb
@@ -1,8 +1,18 @@
 ﻿Imports System.Collections.Generic
 
 Public Class HelloWorldClass
+    ''' <summary>
+    ''' A private member string
+    ''' </summary>
+    ''' <remarks></remarks>
     Private hello_string As String
 
+    ''' <summary>
+    ''' A property to access and set the hello_string string
+    ''' </summary>
+    ''' <value> A string to set as the current string </value>
+    ''' <returns> The current value of the string. </returns>
+    ''' <remarks></remarks>
     Property HelloString As String
         Get
             Return hello_string
@@ -12,12 +22,22 @@ Public Class HelloWorldClass
         End Set
     End Property
 
+    ''' <summary>
+    ''' Used to get access to the length of the hello_world string
+    ''' </summary>
+    ''' <returns> The length of hello_string</returns>
+    ''' <remarks></remarks>
     ReadOnly Property Length() As Integer
         Get
             Return hello_string.Length
         End Get
     End Property
 
+    ''' <summary>
+    ''' Function to return a list of strings, used to learn for each loops
+    ''' </summary>
+    ''' <returns> A list of strings. </returns>
+    ''' <remarks> Nothing special </remarks>
     Public Function hello_func() As List(Of String)
         Dim hello_list As New List(Of String) From {"hello", " ", "world", "!"}
         Return hello_list
